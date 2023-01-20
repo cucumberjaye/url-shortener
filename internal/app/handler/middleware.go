fe2764cbcfd44c47ce04ea9f2ee4dcbeee2c0e62
cucumberjaye <iskander.muzipov@gmail.com>
2023-01-20 19:25:10 +0300

    fix mw

--- internal/app/handler/middleware.go
+++ internal/app/handler/middleware.go
@@ -16,11 +16,6 @@ func (w gzipWriter) Write(data []byte) (int, error) {
 	return w.Writer.Write(data)
 }
 
-type gzipReader struct {
-	body       []byte
-	ReadCloser io.ReadCloser
-}
-
 func (h *Handler) gzipCompress(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
